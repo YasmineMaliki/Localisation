9989a053a975684ad66624ae251eaa147c7b405d
YasmineMaliki <yasminamaliki@gmail.com>
2022-04-27 21:46:23 +0000

    Localisation Commit

---
+++ app/src/main/java/com/iir4/g2/localisation/MainActivity.java
@@ -0,0 +1,193 @@
+package com.iir4.g2.localisation;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private double latitude;
+    private double longitude;
+    private double altitude;
+    private float accuracy;
+    private Button afficher;
+    RequestQueue requestQueue;
+    ImageView logo;
+    String insertUrl = "http://10.0.2.2/localisation/createPosition.php";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        logo = findViewById(R.id.logo);
+
+        Animation aniRotate = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.anim);
+        logo.startAnimation(aniRotate);
+
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10, 150, new
+                LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        latitude = location.getLatitude();
+                        longitude = location.getLongitude();
+                        altitude = location.getAltitude();
+                        accuracy = location.getAccuracy();
+                        @SuppressLint("StringFormatMatches") String msg = String.format(getResources().getString(R.string.new_location), latitude, longitude, altitude, accuracy);
+                        addPosition(latitude, longitude);
+                        Log.d("LL2",msg);
+                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+                       /* String newStatus = "";
+                        switch (status) {
+                            case LocationProvider.OUT_OF_SERVICE:
+                                newStatus = "OUT_OF_SERVICE";
+                                break;
+                            case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                                newStatus = "TEMPORARILY_UNAVAILABLE";
+                                break;
+                            case LocationProvider.AVAILABLE:
+                                newStatus = "AVAILABLE";
+                                break;
+                        }
+                        String msg = String.format(getResources().getString(R.string.provider_new_status),
+                                provider, newStatus);
+                        Log.d("LL",msg);
+                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();*/
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String provider) {
+                        String msg = String.format(getResources().getString(R.string.provider_enabled),
+                                provider);
+                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String provider) {
+                        String msg = String.format(getResources().getString(R.string.provider_disabled),
+                                provider);
+                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                    }
+
+
+
+
+                });
+        afficher= findViewById(R.id.afficher);
+        afficher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("ok","hh");
+                Intent intent = new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        this.finish();
+    }
+    void addPosition(final double lat, final double lon) {
+        StringRequest request = new StringRequest(Request.Method.POST,
+                insertUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("ok","ok");
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("ok",error+"");
+            }
+        }) {
+            @SuppressLint({"MissingPermission", "HardwareIds"})
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                TelephonyManager telephonyManager =
+                        (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+                HashMap<String, String> params = new HashMap<String, String>();
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                params.put("latitude", lat + "");
+                params.put("longitude", lon + "");
+                params.put("date", sdf.format(new Date()) + "");
+                params.put("imei", getImei(getApplicationContext()));
+                return params;
+            }
+        };
+        requestQueue.add(request);
+    }
+
+    private String getImei(Context context) {
+        String deviceId;
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            deviceId = Settings.Secure.getString(
+                    context.getContentResolver(),
+                    Settings.Secure.ANDROID_ID);
+        } else {
+            final TelephonyManager mTelephony = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+            if (mTelephony.getDeviceId() != null) {
+                deviceId = mTelephony.getDeviceId();
+            } else {
+                deviceId = Settings.Secure.getString(
+                        context.getContentResolver(),
+                        Settings.Secure.ANDROID_ID);
+            }
+        }
+
+        return deviceId;
+    }
+}
---
+++ app/src/main/java/com/iir4/g2/localisation/MapsActivity.java
@@ -0,0 +1,94 @@
+package com.iir4.g2.localisation;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.iir4.g2.localisation.databinding.ActivityMapsBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+    String showUrl = "http://10.0.2.2/localisation/loadPositions.php";
+    RequestQueue requestQueue;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        setUpMapIfNeeded();
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+    private void setUpMapIfNeeded() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,
+                showUrl, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray positions = response.getJSONArray("positions");
+                    for (int i = 0; i < positions.length(); i++) {
+                        JSONObject position = positions.getJSONObject(i);
+                        mMap.addMarker(new MarkerOptions().position(new
+                                LatLng(position.getDouble("latitude"),
+                                position.getDouble("longitude"))).title("Marker"));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("ok","no");
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+    }
+}
